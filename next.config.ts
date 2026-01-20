6a7a3b72f6b95cc3282362dba40eb5cfcbca3a23
AltarisV <leon.schuessler@freenet.de>
2026-01-20 02:24:00 +0100

    fix pipeline

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,14 @@
 import type { NextConfig } from 'next';
 
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'OpenHome';
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: '/OpenHome',
+  assetPrefix: '/OpenHome/',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true, // Helps with GitHub Pages routing
+  trailingSlash: true,
 };
 
 export default nextConfig;
